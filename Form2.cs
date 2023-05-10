f08309607e9db6c847c0cadbc43cc78b0d18fe6d
Bbusee <108720581+Bbusee@users.noreply.github.com>
2023-05-10 23:55:09 +0300

    Initial commit

---
+++ Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21015222030_GPvizeOdevi
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public arac arac = null;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            propertyGrid1.SelectedObject = arac;
+        }
+
+        private void OkClicked(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void CancelClicked(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
